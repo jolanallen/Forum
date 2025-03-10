c2ac08b6acd8c074720faed30b318270a2e4ef7f
jolanallen <jolanallen34@gmail.com>
2025-03-10 14:20:14 +0100

    first commit features

--- backend/middlewares/logger.go
+++ backend/middlewares/logger.go
@@ -9,6 +9,6 @@ import (
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		///////////////////////////logic de création de log pour chaque requetes effectuer //////////////////////////////////////////
-		next.ServeHTTP(w, r) // Passer au handler suivant
+		next.ServeHTTP(w, r) // Passer au handler suivant //
 	})
 }
