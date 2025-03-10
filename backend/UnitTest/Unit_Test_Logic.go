a4162384674cc78db84c6e971fd5b3eb01088d3e
zacken-source <marino.chereaudemedina@ynov.com>
2025-03-10 12:50:30 +0100

    test automatisation

--- backend/UnitTest/Unit_Test_Logic.go
+++ backend/UnitTest/Unit_Test_Logic.go
@@ -1,1 +1,34 @@
 package unittest
+
+import (
+	"testing"
+)
+
+func idIsTrue(id int) bool {
+	var isTrue bool = true
+	if id <= 144 {
+		isTrue = false
+	}
+	return isTrue
+}
+
+func TestidIsTrue(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    int
+		expected bool
+	}{
+		{"ID less than or equal to 144", 100, false},
+		{"ID greater than 144", 150, true},
+		{"ID equal to 144", 144, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := idIsTrue(tt.input)
+			if result != tt.expected {
+				t.Errorf("Expected %v, got %v", tt.expected, result)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module Forum
 go 1.24.0
 
 require golang.org/x/crypto v0.36.0
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.0 // indirect
+)
