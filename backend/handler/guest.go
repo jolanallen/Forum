e6994d139738941128fd35c88b00c2e7ca4e7878
Marino <marino.chereaudemedina@ynov.com>
2025-04-15 10:36:17 +0200

    serveur work

--- backend/handler/guest.go
+++ backend/handler/guest.go
@@ -1,96 +1,112 @@
 package handler
-
-import (
-	"Forum/backend/db"
-	"Forum/backend/services"
-	"Forum/backend/structs"
-	"log"
-	"net/http"
-)
-
-func GuestHome(w http.ResponseWriter, r *http.Request) {
-	userID := services.GetUserIDFromSession(r)
-
-	var isAuthenticated bool
-	if userID != 0 {
-		isAuthenticated = true
-	}
-
-	var posts []structs.Post
-
-	err := db.DB.Preload("Comments").Preload("Comments.UserID").Preload("Comments.CommentsLike").Find(&posts).Error
-	if err != nil {
-		log.Println("Erreur lors de la récupération des posts:", err)
-		http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
-		return
-	}
-
-	var categories []structs.Category
-	err = db.DB.Find(&categories).Error
-	if err != nil {
-		log.Println("Erreur lors de la récupération des catégories:", err)
-		http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
-		return
-	}
-
-	var users []structs.User
-	err = db.DB.Find(&users).Error
-	if err != nil {
-		log.Println("Erreur lors de la récupération des utilisateurs:", err)
-		http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
-		return
-	}
-	services.RenderTemplate(w, "forum/home.html", struct {
-		IsAuthenticated bool
-		Posts           []structs.Post
-		Categories      []structs.Category
-		Users           []structs.User
-	}{
-		IsAuthenticated: isAuthenticated,
-		Posts:           posts,
-		Categories:      categories,
-		Users:           users,
-	})
-}
-
-func CategoryHack(w http.ResponseWriter, r *http.Request) {
-	posts, err := services.GetPostsByCategory("hack")
-	if err != nil {
-		http.Error(w, "Erreur lors de la récupération des posts Hack", http.StatusInternalServerError)
-		return
-	}
-	services.RenderTemplate(w, "guest/category_hack.html", posts)
-}
-
-func CategoryProg(w http.ResponseWriter, r *http.Request) {
-	posts, err := services.GetPostsByCategory("prog")
-	if err != nil {
-		http.Error(w, "Erreur lors de la récupération des posts Prog", http.StatusInternalServerError)
-		return
-	}
-	services.RenderTemplate(w, "guest/category_prog.html", posts)
-}
-
-func CategoryNews(w http.ResponseWriter, r *http.Request) {
-	posts, err := services.GetPostsByCategory("news")
-	if err != nil {
-		http.Error(w, "Erreur lors de la récupération des posts News", http.StatusInternalServerError)
-		return
-	}
-	services.RenderTemplate(w, "guest/category_news.html", posts)
-}
-
-func SearchPseudo(w http.ResponseWriter, r *http.Request) {
-	searchQuery := r.URL.Query().Get("query")
-	posts, err := services.SearchPosts(searchQuery)
-	if err != nil {
-		http.Error(w, "Erreur lors de la recherche de posts", http.StatusInternalServerError)
-		return
-	}
-
-	services.RenderTemplate(w, "guest/search.html", posts)
-}
-
-func AboutForum(w http.ResponseWriter, r *http.Request) {
-	services.RenderTemplate(w, "guest/about.html", nil)
-}
+ 
+ import (
+    "Forum/backend/db"
+    "Forum/backend/services"
+    "Forum/backend/structs"
+    "log"
+    "net/http"
+ )
+ 
+ func GuestHome(w http.ResponseWriter, r *http.Request) {
+    userID := services.GetUserIDFromSession(r)
+ 
+    var isAuthenticated bool
+    if userID != 0 {
+        isAuthenticated = true
+    }
+ 
+    var posts []structs.Post
+ 
+    err := db.DB.Preload("Comments").Preload("Comments.UserID").Preload("Comments.CommentsLike").Find(&posts).Error
+    if err != nil {
+        log.Println("Erreur lors de la récupération des posts:", err)
+        http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+        return
+    }
+ 
+    var categories []structs.Category
+    err = db.DB.Find(&categories).Error
+    if err != nil {
+        log.Println("Erreur lors de la récupération des catégories:", err)
+        http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+        return
+    }
+ 
+    user := structs.User{}
+    if isAuthenticated {
+        err = db.DB.First(&user, userID).Error
+        if err != nil {
+            log.Println("Erreur lors de la récupération de l'utilisateur:", err)
+            http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+            return
+        }
+    }
+    var users []structs.User
+    err = db.DB.Find(&users).Error
+    if err != nil {
+        log.Println("Erreur lors de la récupération des utilisateurs:", err)
+        http.Error(w, "Erreur de serveur", http.StatusInternalServerError)
+        return
+    }
+    log.Println("IsAuthenticated:", isAuthenticated)
+    log.Println("Posts:", posts)
+    log.Println("Categories:", categories)
+    log.Println("Users:", users)
+    log.Println("User:", user)
+    services.RenderTemplate(w, "forum/home.html", struct {
+        IsAuthenticated bool
+        Posts           []structs.Post
+        Categories      []structs.Category
+        Users           []structs.User
+        User            structs.User
+    }{
+        IsAuthenticated: isAuthenticated,
+        Posts:           posts,
+        Categories:      categories,
+        Users:           users,
+        User:            user,
+    })
+ }
+ 
+ func CategoryHack(w http.ResponseWriter, r *http.Request) {
+    posts, err := services.GetPostsByCategory("hack")
+    if err != nil {
+        http.Error(w, "Erreur lors de la récupération des posts Hack", http.StatusInternalServerError)
+        return
+    }
+    services.RenderTemplate(w, "guest/category_hack.html", posts)
+ }
+ 
+ func CategoryProg(w http.ResponseWriter, r *http.Request) {
+    posts, err := services.GetPostsByCategory("prog")
+    if err != nil {
+        http.Error(w, "Erreur lors de la récupération des posts Prog", http.StatusInternalServerError)
+        return
+    }
+    services.RenderTemplate(w, "guest/category_prog.html", posts)
+ }
+ 
+ func CategoryNews(w http.ResponseWriter, r *http.Request) {
+    posts, err := services.GetPostsByCategory("news")
+    if err != nil {
+        http.Error(w, "Erreur lors de la récupération des posts News", http.StatusInternalServerError)
+        return
+    }
+    services.RenderTemplate(w, "guest/category_news.html", posts)
+ }
+ 
+ func SearchPseudo(w http.ResponseWriter, r *http.Request) {
+    searchQuery := r.URL.Query().Get("query")
+    posts, err := services.SearchPosts(searchQuery)
+    if err != nil {
+        http.Error(w, "Erreur lors de la recherche de posts", http.StatusInternalServerError)
+        return
+    }
+ 
+    services.RenderTemplate(w, "guest/search.html", posts)
+ }
+ 
+ func AboutForum(w http.ResponseWriter, r *http.Request) {
+    services.RenderTemplate(w, "guest/about.html", nil)
+ }
--- backend/server/routes.go
+++ backend/server/routes.go
@@ -1,67 +1,66 @@
 package server
 
 import (
-	"Forum/backend/handler"
-	"Forum/backend/middlewares"
-	"Forum/backend/services"
-	"net/http"
+    "Forum/backend/handler"
+    "Forum/backend/middlewares"
+    "Forum/backend/services"
+    "net/http"
 
-	"github.com/gorilla/mux"
+    "github.com/gorilla/mux"
 )
 
 func InitRoutes() {
 
-	//////////////////////////////sous router pour guest //////////////////////////////////////////
-	guestRouter := http.NewServeMux()
-	guestRouter.HandleFunc("/", handler.GuestHome)
-	guestRouter.HandleFunc("/hack", handler.CategoryHack)
-	guestRouter.HandleFunc("/prog", handler.CategoryProg)
-	guestRouter.HandleFunc("/news", handler.CategoryNews)
-	guestRouter.HandleFunc("/search", handler.SearchPseudo)
-	guestRouter.HandleFunc("/about", handler.AboutForum)
+    //////////////////////////////sous router pour guest //////////////////////////////////////////
+    guestRouter := http.NewServeMux()
+    guestRouter.HandleFunc("/", handler.GuestHome)
+    guestRouter.HandleFunc("/hack", handler.CategoryHack)
+    guestRouter.HandleFunc("/prog", handler.CategoryProg)
+    guestRouter.HandleFunc("/news", handler.CategoryNews)
+    guestRouter.HandleFunc("/search", handler.SearchPseudo)
+    guestRouter.HandleFunc("/about", handler.AboutForum)
 
-	protectedGuestRouter := middlewares.Logger(middlewares.RateLimit(guestRouter))
-	////////////////////////sous-router pour authentification/////////////////////////////////////
-	authRouter := http.NewServeMux()
-	authRouter.HandleFunc("/login", handler.Login)
-	authRouter.HandleFunc("/register", handler.Register)
-	//authRouter.HandleFunc("/google", handler.Register)
-	//authRouter.HandleFunc("/google", handler.Register)
+    protectedGuestRouter := middlewares.Logger(middlewares.RateLimit(guestRouter))
+    ////////////////////////sous-router pour authentification/////////////////////////////////////
+    authRouter := http.NewServeMux()
+    authRouter.HandleFunc("/login", handler.Login)
+    authRouter.HandleFunc("/register", handler.Register)
+    authRouter.HandleFunc("/google", handler.GoogleLogin)
+    authRouter.HandleFunc("/google/callback", handler.GoogleRegister)
+    protectedAuthRouter := middlewares.Logger(middlewares.RateLimit(authRouter))
+    /////////////////////////sous router pour user ////////////////////////////////////////
+    userRouter := http.NewServeMux()
+    userRouter.HandleFunc("/profile/{id}/edit", handler.UserEditProfile)
+    userRouter.HandleFunc("/posts/news", handler.UserCreatePost)
+    userRouter.HandleFunc("/posts/hack", handler.UserCreatePost)
+    userRouter.HandleFunc("/posts/prog", handler.UserCreatePost)
+    userRouter.HandleFunc("/post/{id}/like", handler.ToggleLikePost)
+    userRouter.HandleFunc("/post/{id}/comment", handler.ToggleLikeComment)
+    userRouter.HandleFunc("/logout", handler.Logout)
+    userRouter.HandleFunc("/profile", handler.UserProfile)
 
-	protectedAuthRouter := middlewares.Logger(middlewares.RateLimit(authRouter))
-	/////////////////////////sous router pour user ////////////////////////////////////////
-	userRouter := http.NewServeMux()
-	userRouter.HandleFunc("/profile/{id}/edit", handler.UserEditProfile)
-	userRouter.HandleFunc("/posts/news", handler.UserCreatePost)
-	userRouter.HandleFunc("/posts/hack", handler.UserCreatePost)
-	userRouter.HandleFunc("/posts/prog", handler.UserCreatePost)
-	userRouter.HandleFunc("/post/{id}/like", handler.ToggleLikePost)
-	userRouter.HandleFunc("/post/{id}/comment", handler.ToggleLikeComment)
-	userRouter.HandleFunc("/logout", handler.Logout)
-	userRouter.HandleFunc("/profile", handler.UserProfile)
+    protectedUserRouter := middlewares.Logger(middlewares.Authentication(middlewares.RateLimit(userRouter))) //(protégées par le middlewares Authentication
 
-	protectedUserRouter := middlewares.Logger(middlewares.Authentication(middlewares.RateLimit(userRouter))) //(protégées par le middlewares Authentication
+    ///////////////////////// sous routeur mour Admin ////////////////////////////////////////////////
+    adminRouter := mux.NewRouter()
+    adminRouter.HandleFunc("/dashboard", handler.AdminDashboard)
+    adminRouter.HandleFunc("/user/{id}/delete", handler.AdminDeleteUser)
+    adminRouter.HandleFunc("/comment/{id}/delete", handler.AdminDeleteComment)
+    adminRouter.HandleFunc("/post/{id}/delete", handler.AdminDeletePost)
 
-	///////////////////////// sous routeur mour Admin ////////////////////////////////////////////////
-	adminRouter := mux.NewRouter()
-	adminRouter.HandleFunc("/dashboard", handler.AdminDashboard)
-	adminRouter.HandleFunc("/user/{id}/delete", handler.AdminDeleteUser)
-	adminRouter.HandleFunc("/comment/{id}/delete", handler.AdminDeleteComment)
-	adminRouter.HandleFunc("/post/{id}/delete", handler.AdminDeletePost)
+    protectedAdminRouter := middlewares.Logger(
+        middlewares.Authentication(
+            middlewares.AdminAuthorization(adminRouter),
+        ),
+    ) //(protégées par les middelwares AdminAuthorization + Authentication
+    ///////////////////////// routeur Principal ////////////////////////////////////////////////
 
-	protectedAdminRouter := middlewares.Logger(
-		middlewares.Authentication(
-			middlewares.AdminAuthorization(adminRouter),
-		),
-	) //(protégées par les middelwares AdminAuthorization + Authentication
-	///////////////////////// routeur Principal ////////////////////////////////////////////////
-
-	mainRouter := http.NewServeMux()
-	mainRouter.Handle("/forum/", http.StripPrefix("/forum", protectedGuestRouter))
-	mainRouter.Handle("/auth/", http.StripPrefix("/auth", protectedAuthRouter))
-	mainRouter.Handle("/user/", http.StripPrefix("/user", protectedUserRouter))
-	mainRouter.Handle("/admin/", http.StripPrefix("/admin", protectedAdminRouter))
-		
-	services.F.MainRouter = mainRouter /////////ajout du routeur principal a la structure globlal Forum
+    mainRouter := http.NewServeMux()
+    mainRouter.Handle("/forum/", http.StripPrefix("/forum", protectedGuestRouter))
+    mainRouter.Handle("/auth/", http.StripPrefix("/auth", protectedAuthRouter))
+    mainRouter.Handle("/user/", http.StripPrefix("/user", protectedUserRouter))
+    mainRouter.Handle("/admin/", http.StripPrefix("/admin", protectedAdminRouter))
+        
+    services.F.MainRouter = mainRouter /////////ajout du routeur principal a la structure globlal Forum
 
 }
