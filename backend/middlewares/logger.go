3850192f7023d270433fd242a89e150151f4be17
jolanallen <jolanallen34@gmail.com>
2025-04-16 23:06:57 +0200

    resolve conflict

--- backend/middlewares/logger.go
+++ backend/middlewares/logger.go
@@ -8,11 +8,16 @@ import (
 
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+<<<<<<< HEAD
 		ip := r.RemoteAddr
 		if strings.Contains(r.Header.Get("X-Forwarded-For"), ",") {
 			ip = strings.Split(r.Header.Get("X-Forwarded-For"), ",")[0]
 		}
 		log.Printf("[Acess] => IP: %s | Method: %s | URL: %s | User-Agent: %s", ip, r.Method, r.URL.Path, r.UserAgent())
 		next.ServeHTTP(w, r)
+=======
+		///////////////////////////logic de création de log pour chaque requetes effectuer //////////////////////////////////////////
+		next.ServeHTTP(w, r) // Passer au handler suivant //
+>>>>>>> c2ac08b (first commit features)
 	})
 }
